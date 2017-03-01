e6ed0d0a99b5724e0b8bc2ae4682011fe555161c
smmakowski <makowski.sm@gmail.com>
2017-03-01 12:24:34 -0800

    Pass tests for pluckFirstLine From FileAsync; woking on getstatuscodeAsync

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -8,14 +8,39 @@ var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
 
+
+
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+
+  setTimeout(function(){
+  	fs.readFile(filePath, 'utf8', function(err, content){
+  		if (err) {
+  			reject(err);
+  		} else {
+  			resolve(content.split('\n')[0]);
+  		}
+  	})
+  }, 200);
+ });
+  
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  return new Promise(function(resolve,reject) {
+  	setTimeout(function(){
+  		request.get(url, function(err, statusCode){
+  			if (err) {
+  				reject(404);
+  			} else {
+  				resolve(200);
+  			}
+  		})
+  	})
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
